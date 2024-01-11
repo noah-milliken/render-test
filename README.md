bd4d91cf0f72b50e6ea2d6fc899389500cec599c
noah milliken <noahmilliken@gmail.com>
2024-01-10 21:39:33 -0500

    broke server into multiple parts.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # render-test
+
+let notes = [
+{
+id: 1,
+content: "HTML is easy",
+important: true,
+},
+{
+id: 2,
+content: "Browser can execute only JavaScript",
+important: false,
+},
+{
+id: 3,
+content: "GET and POST are the most important methods of HTTP protocol",
+important: true,
+},
+];
--- otherServers/mongo.js
+++ otherServers/mongo.js
@@ -1,30 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-// if (process.argv.length < 3) {
-//   console.log("give password as argument");
-//   process.exit(1);
-// }
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-// const password = process.argv[2];
+const password = process.argv[2]
 
-const url = process.env.MONGO_URI;
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/notesApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
+// eslint-disable-next-line no-unused-vars
 const note = new Note({
-  content: "HTML is Easy",
+  content: 'Mongoose makes things easy',
+  date: new Date(),
   important: true,
-});
+})
 
-note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
