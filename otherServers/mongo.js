ec2cf45456a8e25395764c57b618725e07a295d5
noah milliken <noahmilliken@gmail.com>
2024-01-11 17:14:14 -0500

    created tests and helper functions added the express-async-errors package for concise error handling.

--- otherServers/mongo.js
+++ otherServers/mongo.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
 
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/notesApp?retryWrites=true&w=majority`
-
+console.log(url)
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
@@ -23,21 +15,21 @@ const Note = mongoose.model('Note', noteSchema)
 
 // eslint-disable-next-line no-unused-vars
 const note = new Note({
-  content: 'Mongoose makes things easy',
+  content: 'My mom is a skilled mountain climber.',
   date: new Date(),
   important: true,
 })
 
-/*
+
 note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
 })
-*/
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
